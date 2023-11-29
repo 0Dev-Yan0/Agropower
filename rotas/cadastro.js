c1f98af1b3c944609eaa0bc41b8f0b9d3afcc4f2
Yan <135740699+0DevYan0@users.noreply.github.com>
2023-11-29 16:25:22 -0300

    route-configuration

--- rotas/cadastro.js
+++ rotas/cadastro.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
+const users = [ ]; //! Lista que vai armazenar os dados recebidos da requisição antes enviar eles;
 
 const rotaCadastro = Router();
 
 rotaCadastro.post("/", (req,res) => {
+    console.log(req);
+    // users.push()
     res.send({ mensagem: "Cadastro feito com sucesso!" });
 });
 
 module.exports = rotaCadastro;
+
+//! Propósito de envio automatizado para o banco de dados;
--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 const rotaCadastro = require("./rotas/cadastro");
 const rotaLogin = require("./rotas/login");
 const rotaApi = require("./rotas/api")
+const rotaEstoque = require("./rotas/estoque");
 //! Constantes de rotas--->
 
 const express = require("express"); //! Trazendo o express ao projeto;
 const app = express(); //! Criação do servidor para uso adjacente;
 const porta = process.env.PORT || 8000; //! Caso não encontre uma porta aberta se direcione para a 8000 disponível;
-const users = [ ];
-const produtos = [ ];
 
 app.use("/api", rotaApi); //! Rota base da api que envia um objeto JSON e se conecta diretamente com o Front;
-app.use("/cadastro", rotaCadastro);
-app.use("/login", rotaLogin);
+app.use("/cadastro", rotaCadastro); //! Rota feita para a passagem dos dados de cadastro;
+app.use("/login", rotaLogin); //! Rota para a conferência de login dos usuaários;
+app.use("/estoque", rotaEstoque); //! Rota para alteração e envio de dados pelos arquivos JSON do estoque;
 
 app.listen(porta, () => {
     console.log(`Escutando a porta: ${porta}`);
