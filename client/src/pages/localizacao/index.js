91c322fb11096c2ed8a3dcd0ad61974b3b3c9061
CarlosEsteves406 <135874851+CarlosEsteves406@users.noreply.github.com>
2023-11-30 14:31:29 -0300

    miau

---
+++ client/src/pages/localizacao/index.js
@@ -0,0 +1,14 @@
+import lupa from '../'
+
+function localizacao() {
+    return (
+        <div className='localizacao_produtor'>
+            <header></header>
+            <p>Localização dos fornecedores</p>
+            <div>
+                <input placeholder='Buscar fornecedor'></input>
+                <button src={}></button>
+            </div>
+        </div>
+    )
+}
